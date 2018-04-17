90221c9146aefc221c3ad5f158f0a3368bb1892b
Paislee House <pshouse13@gmail.com>
2018-04-17 08:17:45 -0500

    working on blogz

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'y337kGcys&zP3B'
@@ -14,6 +14,7 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(200))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, title, body):
         self.title = title
@@ -25,7 +26,29 @@ class Blog(db.Model):
         else:
             return False
 
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15))
+    password = db.Column(db.String(20))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
 #begin handlers
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    
+
+@app.route('/signup', methods=['POST'])
+def signup():
+
+
+
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
 
@@ -41,10 +64,12 @@ def blog():
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
 
+    owner = User.query.filter_by(email=session['email']).first()
+
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-        new_entry = Blog(title, body)
+        new_entry = Blog(title, body, owner)
 
         if new_entry.validate_entry():
             db.session.add(new_entry)
